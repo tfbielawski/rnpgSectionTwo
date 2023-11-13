272268374f0a02f88ff54df08974f0c379ca23b3
Tom Bielawski <70349084+tfbielawski@users.noreply.github.com>
2023-11-13 18:33:30 -0500

    Section 5, Leson 88: Platform API

--- rnpgSectionTwo/components/ui/Title.js
+++ rnpgSectionTwo/components/ui/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native";
+import {Text, StyleSheet, Platform} from "react-native";
 import Colors from "../../constants/colors";
 
 //Instead of props > props.children
@@ -14,7 +14,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: Colors.font500,
     textAlign: "center",
-    borderWidth: 2,
+    // borderWidth: Platform.OS === "android" ? 3 : 1,
+    borderWidth: Platform.select({android: 3, ios: 1}),
+    // borderWidth: 1,
     borderColor: Colors.accent500,
     padding: 12,
     maxWidth: "80%",
